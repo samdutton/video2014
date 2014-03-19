35a0d2caf55caf6cd091c83267aec64defc7589f
samdutton <samdutton@gmail.com>
2014-03-19 14:54:00 +0000

    Basic template

---
+++ slide_config.js
@@ -0,0 +1,48 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'State of HTML5 Video',
+    eventTitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    analytics: 'UA-44413410-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'IM Fell Great Primer',
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['custom'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+    {
+    name: 'Mark Robertson',
+    company: 'ReelSEO',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: ''
+    },
+    {
+    name: 'Sam Dutton',
+    company: 'Developer Advocate, Google Chrome',
+    gplus: 'http://plus.samdutton.com',
+    twitter: '@sw12',
+    www: 'http://www.samdutton.com',
+    github: 'http://github.com/samdutton'
+    },
+    {
+    name: 'Jeroen Wijering',
+    company: 'JW Player',
+    gplus: '',
+    twitter: '',
+    www: '',
+    github: ''
+    }
+  ]
+};
+
